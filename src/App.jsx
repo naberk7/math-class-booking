273ac1d3075a2bf32f609753e9ed898fac4e4573
Berkay Ediş <n_aberk@MacBook-Air-de-Berkay.local>
2025-11-27 21:43:04 +0300

    Nasıl Çalışır sekmesi - syntax hatası düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -636,8 +636,9 @@ const toggleSlotAvailability = async (day, time) => {
             <p>• Çalışma Saatleri: <strong>08:00 - 22:00</strong></p>
           </div>
         </div>
-    
-)}
+        )}
+      </div>
+
 
 {/* Nasıl Çalışır Sayfası */}
 {viewMode === 'info' && (
