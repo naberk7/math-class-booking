2b76a426be39ba93f40af32115ddeab1cf4bb781
Berkay Ediş <n_aberk@MacBook-Air-de-Berkay.local>
2025-11-27 20:40:43 +0300

    supabaseClient space hatası düzeltildi

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://edxnltxzalqudizbxocf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeG5sdHh6YWxxdWRpemJ4b2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTk5NTgsImV4cCI6MjA3OTgzNTk1OH0.4ISEVQEZ0FheQulGd4QXl51ugmlZOGs9to1-UHuPVfs
-'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeG5sdHh6YWxxdWRpemJ4b2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTk5NTgsImV4cCI6MjA3OTgzNTk1OH0.4ISEVQEZ0FheQulGd4QXl51ugmlZOGs9to1-UHuPVfs'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
